83f0796b559608dfb56558414a8ecfca91b8f98a
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2018-08-22 13:28:46 +0100

    Changed template call block to include variables

--- envelope/letter/template.js
+++ envelope/letter/template.js
@@ -6,7 +6,8 @@ platform.core.node({
   path: '/mailjet/envelope/letter/template',
   public: false,
   inputs: [
-    'templateID'
+    'id',
+    'variables'
   ],
   outputs: [
     'templateLetter'
@@ -14,17 +15,19 @@ platform.core.node({
   hints: {
     node: 'Construct a letter from a template by using <span class="hl-blue">templateID</span>.',
     inputs: {
-      templateID: 'The template id of the template to be used',
+      id: 'The template id of the template to be used',
+      variables: 'The template variables of the template to be used',
     },
     outputs: {
-      letter: '<span class="hl-blue">Template letter</span> to be sealed in an envelope.',
+      letter: 'A <span class="hl-blue">Template letter</span> to be sealed in an envelope.',
     },
   }
 }, (inputs, output, control) => {
   output('templateLetter',
     {
-      'Mj-TemplateID': inputs.templateID,
-      'Mj-TemplateLanguage': true
+      'Mj-TemplateID': inputs.id,
+      'Mj-TemplateLanguage': true,
+      'Vars': inputs.variables,
     }
   );
 });
